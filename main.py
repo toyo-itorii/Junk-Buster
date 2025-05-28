0ecdfc651c27ef6d497b26978713ae3c13f22626
toyo-itorii <toyo-itori@outlook.com>
2025-05-28 20:57:17 +0200

    Get dataset from Kaggle

---
+++ main.py
@@ -0,0 +1,6 @@
+# #Phase 2: Data Loading & Initial Exploration
+# Load the Dataset:
+
+# Write a Python script (e.g., data_loader.py or directly in main.py) to load your dataset into a Pandas DataFrame.
+# Handle different file formats (e.g., CSV, TSV).
+# Example for CSV: df = pd.read_csv('data/sms_spam_collection.csv', encoding='latin-1') (encoding often needed for text datasets).
